fd714cb1a50e8cbff78c1fff681222c61d8203d1
Daniel Schruhl <danielschruhl@gmail.com>
2018-09-07 08:08:08 +0200

    write dataset to hdfs

--- src/main/scala/com/github/meandor/Orion.scala
+++ src/main/scala/com/github/meandor/Orion.scala
@@ -1,12 +1,16 @@
 package com.github.meandor
 
+import com.typesafe.scalalogging.LazyLogging
 import org.apache.spark.sql.SparkSession
 
-object Orion {
+object Orion extends LazyLogging {
 
   val spark: SparkSession = SparkSession.builder.appName("Orion").getOrCreate()
 
   def main(args: Array[String]): Unit = {
     val trainDataset = Features.loadDataset(spark, "hdfs://ip-172-31-38-13.eu-central-1.compute.internal:8020/grocery/train.csv")
+
+    logger.info("Writing transformed dataset")
+    trainDataset.write.csv("hdfs://ip-172-31-38-13.eu-central-1.compute.internal:8020/grocery/train-transformed.csv")
   }
 }
